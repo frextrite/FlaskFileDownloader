3e0f8e7fe67f21c5b5b31c1618b63fa4c1d53d94
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-04 10:54:17 +0530

    Fix code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 
 from flask import Flask, send_from_directory
@@ -8,9 +7,11 @@ app = Flask(__name__)
 app.config['UPLOAD_DIRECTORY'] = './'
 filename = "easter_egg"
 
+
 @app.route('/download', methods=['GET'])
 def get_file():
     return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename, as_attachment=True)
+
 
 @app.route('/time/<filename>')
 def get_current_time_file(filename):
@@ -20,6 +21,7 @@ def get_current_time_file(filename):
         f.write(current_time)
 
     return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=27015)
